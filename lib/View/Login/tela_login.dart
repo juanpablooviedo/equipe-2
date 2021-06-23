5157c2c3276c60e972f749c223d6eb3c2387cde7
Ednacio da Silva <ednacio.ssouza@gmail.com>
2021-06-23 15:34:21 -0300

    alteracao e criacao da tela login

--- lib/View/Login/tela_login.dart
+++ lib/View/Login/tela_login.dart
@@ -10,6 +10,29 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFA901F7),
+      body: Container(
+        height: 360,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(200),
+              bottomRight: Radius.circular(200)),
+        ),
+        child: Column(
+          children: [
+            Text(
+              "Lovepeople",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF3101B9)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:todo_lovepeople/View/Login/tela_login.dart';
 
 main() {
-  runApp(LoginPage());
+  runApp(MaterialApp(
+    home: LoginPage(),
+  ));
 }
