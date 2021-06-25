d0b61678217025a5f1f9b96b0a4a266a5c1c28ef
diogoflutter <diogodfp@gmail.com>
2021-06-25 11:40:22 -0300

    segundo commit

---
+++ lib/View/Sucesso de Cadastro/tela_sucesso_cadastro.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class RegisterSuccess extends StatefulWidget {
+  const RegisterSuccess({Key? key, required String home}) : super(key: key);
+
+  @override
+  _RegisterSuccessState createState() => _RegisterSuccessState();
+}
+
+class _RegisterSuccessState extends State<RegisterSuccess> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.bottomCenter,
+          children: <Widget>[
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 280,
+                      bottom: 30,
+                    ),
+                    child: Text(
+                      "Cadastro Concluído!",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: "Montserrat-SemiBold"),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Começar",
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat-SemiBold"),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xFF3101B9)),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          side: BorderSide(color: Colors.white, width: 2),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFA901F7),
+              ),
+            ),
+            Container(
+              width: 500,
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(210),
+                  topRight: Radius.circular(210),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                bottom: 20,
+              ),
+              child: Text(
+                "Os ventos da programação estão indo até você",
+                style: TextStyle(
+                  color: Color(0xFF3101B9),
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_lovepeople/View/Sucesso%20de%20Cadastro/tela_sucesso_cadastro.dart';
 
 main () {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'lovepeople',
+      home: RegisterSuccess(home: '',),
     );
   }
 }
