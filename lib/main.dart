81a0fc6dafa876afa8399f2f33582f06f1e8f0e3
Anita Spieker <63751505+anitaspieker@users.noreply.github.com>
2021-06-27 23:07:18 +0200

    Delete main.dart

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:todo_lovepeople/View/Login/tela_login.dart';
-
-main() {
-  runApp(MaterialApp(
-    home: LoginPage(),
-  ));
-}
