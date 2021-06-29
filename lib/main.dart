63865000f828bd3f1a72e441f221190da6231971
KazuoSojima <kazuosojima@gmail.com>
2021-06-28 21:00:39 -0300

    Merge branch 'master' into feature/sojima/tela_lista

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:todo_lovepeople/View/Listagem%20de%20Tarefas/tela_listagem_tarefas.dart';
-=======
-import 'package:todo_lovepeople/View/Cadastro%20de%20Tarefa/tela_cadastro_tarefa.dart';
->>>>>>> master
 
 main() {
   runApp(MyApp());
@@ -15,12 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'lovepeople',
-<<<<<<< HEAD
       home: TelaTarefas(),
-=======
-      home: CadastroTarefa(),
-      debugShowCheckedModeBanner: false,
->>>>>>> master
     );
   }
 }
