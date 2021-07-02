c71dca89b7c464707d39c38718b624896ccbdc8f
KazuoSojima <kazuosojima@gmail.com>
2021-07-01 21:31:30 -0300

    alteração de layout e API

---
+++ lib/Model/Listagem de Tarefas/API_listagem_tarefas.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:todo_lovepeople/Model/Listagem%20de%20Tarefas/listagemTarefa.dart';
+
+class ListagemTarefaRepository {
+  Future<ListaTarefa> listagemTarefa() async {
+    var uri = Uri.parse('https://todo-lovepeople.herokuapp.com/todos');
+    var response = await http.get(uri, headers: {
+      HttpHeaders.authorizationHeader: "Bearer {token}",
+    });
+    var json = jsonDecode(response.body);
+    return ListaTarefa.fromJson(json);
+  }
+}
