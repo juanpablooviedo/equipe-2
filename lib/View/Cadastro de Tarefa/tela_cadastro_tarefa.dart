88ddc9b0f758434c3c61538c18a98f3965a9ff70
anitaspieker <nitamspieker@gmail.com>
2021-07-07 20:53:38 +0200

    Tela de Cadastro concertada

--- lib/View/Cadastro de Tarefa/tela_cadastro_tarefa.dart
+++ lib/View/Cadastro de Tarefa/tela_cadastro_tarefa.dart
@@ -38,216 +38,219 @@ class _CadastroTarefaState extends State<CadastroTarefa> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Color.fromRGBO(169, 1, 247, 1),
-      body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Container(
-                          height: 100,
-                          width: 100,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              bottomRight: Radius.circular(100),
-                            ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(100),
                           ),
                         ),
-                        Container(
-                          margin: EdgeInsets.only(
-                            right: 20,
-                            bottom: 20,
-                          ),
-                          child: Image.asset(
-                            'assets/image/logo_lovepeople.png',
-                            height: 55,
-                            width: 55,
-                          ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(
+                          right: 20,
+                          bottom: 20,
                         ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: 55,
-                  ),
-                  Text(
-                    'Nova Tarefa',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat-SemiBold',
-                      fontSize: 25,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: Container(
-                  height: 55,
-                  width: 622,
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.only(
-                    left: 25,
-                    right: 25,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: TextFormField(
-                    controller: _ctrlTitle,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Container(
-                          height: 25,
-                          alignment: Alignment.center,
-                          child: Text(
-                            'O campo do título é obrigatório!',
-                            style: TextStyle(
-                              fontFamily: "Tahoma",
-                              fontSize: 14,
-                            ),
-                          ),
-                        )));
-                      }
-                    },
-                    style: TextStyle(
-                      fontFamily: 'Tahoma',
-                      fontSize: 25,
-                      color: Color(0xFF3101B9),
-                    ),
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.zero,
-                      hintText: 'Título da Tarefa',
-                      hintStyle: TextStyle(
-                        color: Color(0xFF3101B9),
+                        child: Image.asset(
+                          'assets/image/logo_lovepeople.png',
+                          height: 55,
+                          width: 55,
+                        ),
                       ),
-                      border: OutlineInputBorder(borderSide: BorderSide.none),
-                    ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Center(
-                child: Container(
-                  height: 300,
-                  width: 622,
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.only(
-                    left: 25,
-                    right: 25,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: TextFormField(
-                    controller: _ctrlDescription,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Container(
-                          height: 25,
-                          alignment: Alignment.center,
-                          child: Text(
-                            'O campo da descrição é obrigatório!',
-                            style: TextStyle(
-                              fontFamily: "Tahoma",
-                              fontSize: 14,
-                            ),
-                          ),
-                        )));
-                      }
-                    },
-                    style: TextStyle(
-                      fontFamily: 'Tahoma',
-                      fontSize: 18,
-                      color: Color.fromRGBO(49, 1, 185, 1).withOpacity(.5),
-                    ),
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.zero,
-                      hintText: 'Escreva uma descrição para sua tarefa.',
-                      hintStyle: TextStyle(
-                        color: Color.fromRGBO(49, 1, 185, 1).withOpacity(.5),
-                      ),
-                      border: OutlineInputBorder(borderSide: BorderSide.none),
-                    ),
+                SizedBox(
+                  width: 55,
+                ),
+                Text(
+                  'Nova Tarefa',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat-SemiBold',
+                    fontSize: 25,
+                    color: Colors.white,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                height: 50,
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Container(
+                height: 55,
+                width: 622,
+                padding: EdgeInsets.all(10),
                 margin: EdgeInsets.only(
                   left: 25,
                   right: 25,
                 ),
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    colorButton(0xFFFFF2CC),
-                    colorButton(0xFFffD0BD),
-                    colorButton(0xFFE8C5FF),
-                    colorButton(0xFFCAFBFF),
-                    colorButton(0xFFE3FFE6),
-                    colorButton(0xFFffeaa7),
-                    colorButton(0xFFfab1a0),
-                    colorButton(0xFFa29bfe),
-                    colorButton(0xFF74b9ff),
-                    colorButton(0xFF55efc4),
-                  ],
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextFormField(
+                  controller: _ctrlTitle,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Container(
+                        height: 25,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'O campo do título é obrigatório!',
+                          style: TextStyle(
+                            fontFamily: "Tahoma",
+                            fontSize: 14,
+                          ),
+                        ),
+                      )));
+                    }
+                  },
+                  style: TextStyle(
+                    fontFamily: 'Tahoma',
+                    fontSize: 25,
+                    color: Color(0xFF3101B9),
+                  ),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.zero,
+                    hintText: 'Título da Tarefa',
+                    hintStyle: TextStyle(
+                      color: Color(0xFF3101B9),
+                    ),
+                    border: OutlineInputBorder(borderSide: BorderSide.none),
+                  ),
                 ),
               ),
-              SizedBox(
-                height: 60,
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    alignment: Alignment.centerLeft,
-                    icon: Icon(
-                      Icons.close_rounded,
-                      size: 100,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Center(
+              child: Container(
+                height: 300,
+                width: 622,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.only(
+                  left: 25,
+                  right: 25,
+                ),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextFormField(
+                  controller: _ctrlDescription,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Container(
+                        height: 25,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'O campo da descrição é obrigatório!',
+                          style: TextStyle(
+                            fontFamily: "Tahoma",
+                            fontSize: 14,
+                          ),
+                        ),
+                      )));
+                    }
+                  },
+                  style: TextStyle(
+                    fontFamily: 'Tahoma',
+                    fontSize: 18,
+                    color: Color.fromRGBO(49, 1, 185, 1).withOpacity(.5),
+                  ),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.zero,
+                    hintText: 'Escreva uma descrição para sua tarefa.',
+                    hintStyle: TextStyle(
+                      color: Color.fromRGBO(49, 1, 185, 1).withOpacity(.5),
                     ),
-                    color: Colors.white,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
+                    border: OutlineInputBorder(borderSide: BorderSide.none),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 50,
+              margin: EdgeInsets.only(
+                left: 25,
+                right: 25,
+              ),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  colorButton(0xFFFFF2CC),
+                  colorButton(0xFFffD0BD),
+                  colorButton(0xFFE8C5FF),
+                  colorButton(0xFFCAFBFF),
+                  colorButton(0xFFE3FFE6),
+                  colorButton(0xFFffeaa7),
+                  colorButton(0xFFfab1a0),
+                  colorButton(0xFFa29bfe),
+                  colorButton(0xFF74b9ff),
+                  colorButton(0xFF55efc4),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: IconButton(
+                      alignment: Alignment.centerLeft,
+                      icon: Icon(
+                        Icons.close_rounded,
+                        size: 100,
+                      ),
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
                   ),
                   Expanded(
-                    child: Container(),
-                  ),
-                  IconButton(
-                    alignment: Alignment.center,
-                    icon: Icon(
-                      Icons.check_rounded,
-                      size: 100,
+                    child: IconButton(
+                      alignment: Alignment.center,
+                      icon: Icon(
+                        Icons.check_rounded,
+                        size: 100,
+                      ),
+                      color: Colors.white,
+                      onPressed: () {
+                        print('ONPRESSED');
+                        _clickButton(context);
+                      },
                     ),
-                    color: Colors.white,
-                    onPressed: () {
-                      print('ONPRESSED');
-                      _clickButton(context);
-                    },
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        initialRoute: "login",
+        initialRoute: "telaCadastroTarefa",
         routes: {
           "login": (context) => LoginPage(),
           "telaCadastro": (context) => RegisterUser(),
