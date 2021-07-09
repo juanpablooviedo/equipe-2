8ba5d21d2d55245d0825641b040fdcbbf58f87d3
Ednacio da silva <ednacio.ssouza@gmail.com>
2021-07-09 20:37:22 -0300

    Finalizacao do app

--- lib/View/Cadastro de Tarefa/tela_cadastro_tarefa.dart
+++ lib/View/Cadastro de Tarefa/tela_cadastro_tarefa.dart
@@ -22,7 +22,9 @@ class _CadastroTarefaState extends State<CadastroTarefa> {
           right: 25,
         ),
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            _ctrlColor = color.toString();
+          },
           child: Text(''),
           style: ElevatedButton.styleFrom(
             primary: Color(color),
@@ -38,202 +40,230 @@ class _CadastroTarefaState extends State<CadastroTarefa> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Color.fromRGBO(169, 1, 247, 1),
-      body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Container(
-                          height: 100,
-                          width: 100,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              bottomRight: Radius.circular(100),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(
-                            right: 20,
-                            bottom: 20,
-                          ),
-                          child: Image.asset(
-                            'assets/image/logo_lovepeople.png',
-                            height: 55,
-                            width: 55,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: 55,
-                  ),
-                  Text(
-                    'Nova Tarefa',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat-SemiBold',
-                      fontSize: 25,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: Container(
-                  height: 55,
-                  width: 622,
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.only(
-                    left: 25,
-                    right: 25,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: TextFormField(
-                    controller: _ctrlTitle,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Esse campo é obrigatório!';
-                      }
-                    },
-                    style: TextStyle(
-                      fontFamily: 'Tahoma',
-                      fontSize: 25,
-                      color: Color(0xFF3101B9),
-                    ),
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.zero,
-                      hintText: 'Titulo da Tarefa',
-                      hintStyle: TextStyle(
-                        color: Color(0xFF3101B9),
-                      ),
-                      border: OutlineInputBorder(borderSide: BorderSide.none),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Center(
-                child: Container(
-                  height: 300,
-                  width: 622,
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.only(
-                    left: 25,
-                    right: 25,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: TextFormField(
-                    controller: _ctrlDescription,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Esse campo é obrigatório!';
-                      }
-                    },
-                    style: TextStyle(
-                      fontFamily: 'Tahoma',
-                      fontSize: 18,
-                      color: Color.fromRGBO(49, 1, 185, 1).withOpacity(.5),
-                    ),
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.zero,
-                      hintText: 'Escreva uma descirção para sua tarefa.',
-                      hintStyle: TextStyle(
-                        color: Color.fromRGBO(49, 1, 185, 1).withOpacity(.5),
-                      ),
-                      border: OutlineInputBorder(borderSide: BorderSide.none),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                height: 50,
-                margin: EdgeInsets.only(
-                  left: 25,
-                  right: 25,
-                ),
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    colorButton(0xFFFFF2CC),
-                    colorButton(0xFFffD0BD),
-                    colorButton(0xFFE8C5FF),
-                    colorButton(0xFFCAFBFF),
-                    colorButton(0xFFE3FFE6),
-                    colorButton(0xFFffeaa7),
-                    colorButton(0xFFfab1a0),
-                    colorButton(0xFFa29bfe),
-                    colorButton(0xFF74b9ff),
-                    colorButton(0xFF55efc4),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Center(
-                child: Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Stack(
+                    alignment: Alignment.center,
                     children: [
-                      IconButton(
-                        icon: Icon(
-                          Icons.close,
-                          size: 100,
+                      Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(100),
+                          ),
                         ),
-                        color: Colors.white,
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
                       ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.check,
-                          size: 100,
+                      Container(
+                        margin: EdgeInsets.only(
+                          right: 20,
+                          bottom: 20,
                         ),
-                        color: Colors.white,
-                        onPressed: () {
-                          print('ONPRESSED');
-                          _clickButton(context);
-                          Navigator.pushReplacementNamed(
-                              context, 'listaTarefa');
-                        },
+                        child: Image.asset(
+                          'assets/image/logo_lovepeople.png',
+                          height: 55,
+                          width: 55,
+                        ),
                       ),
                     ],
                   ),
                 ),
+                SizedBox(
+                  width: 55,
+                ),
+                Text(
+                  'Nova Tarefa',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat-SemiBold',
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Container(
+                height: 55,
+                width: 622,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.only(
+                  left: 25,
+                  right: 25,
+                ),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextFormField(
+                  controller: _ctrlTitle,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Container(
+                        height: 25,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'O campo do título é obrigatório!',
+                          style: TextStyle(
+                            fontFamily: "Tahoma",
+                            fontSize: 14,
+                          ),
+                        ),
+                      )));
+                    }
+                  },
+                  style: TextStyle(
+                    fontFamily: 'Tahoma',
+                    fontSize: 25,
+                    color: Color(0xFF3101B9),
+                  ),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.zero,
+                    hintText: 'Título da Tarefa',
+                    hintStyle: TextStyle(
+                      color: Color(0xFF3101B9),
+                    ),
+                    border: OutlineInputBorder(borderSide: BorderSide.none),
+                  ),
+                ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Center(
+              child: Container(
+                height: 300,
+                width: 622,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.only(
+                  left: 25,
+                  right: 25,
+                ),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextFormField(
+                  controller: _ctrlDescription,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Container(
+                        height: 25,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'O campo da descrição é obrigatório!',
+                          style: TextStyle(
+                            fontFamily: "Tahoma",
+                            fontSize: 14,
+                          ),
+                        ),
+                      )));
+                    }
+                  },
+                  style: TextStyle(
+                    fontFamily: 'Tahoma',
+                    fontSize: 18,
+                    color: Color.fromRGBO(49, 1, 185, 1).withOpacity(.5),
+                  ),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.zero,
+                    hintText: 'Escreva uma descrição para sua tarefa.',
+                    hintStyle: TextStyle(
+                      color: Color.fromRGBO(49, 1, 185, 1).withOpacity(.5),
+                    ),
+                    border: OutlineInputBorder(borderSide: BorderSide.none),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 50,
+              margin: EdgeInsets.only(
+                left: 25,
+                right: 25,
+              ),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  colorButton(0xFFFFF2CC),
+                  colorButton(0xFFffD0BD),
+                  colorButton(0xFFE8C5FF),
+                  colorButton(0xFFCAFBFF),
+                  colorButton(0xFFE3FFE6),
+                  colorButton(0xFFffeaa7),
+                  colorButton(0xFFfab1a0),
+                  colorButton(0xFFa29bfe),
+                  colorButton(0xFF74b9ff),
+                  colorButton(0xFF55efc4),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: IconButton(
+                      alignment: Alignment.centerLeft,
+                      icon: Icon(
+                        Icons.close_rounded,
+                        size: 100,
+                      ),
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: IconButton(
+                      alignment: Alignment.center,
+                      icon: Icon(
+                        Icons.check_rounded,
+                        size: 100,
+                      ),
+                      color: Colors.white,
+                      onPressed: () {
+                        print('ONPRESSED');
+                        _clickButton(context);
+                        Navigator.pushReplacementNamed(context, 'listaTarefa');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
   void _clickButton(BuildContext context) {
+    bool formOk = _formKey.currentState!.validate();
+    if (!formOk) {
+      return;
+    }
     String title = _ctrlTitle.text;
     String description = _ctrlDescription.text;
     String color = _ctrlColor;
--- lib/View/Recuperar Login e Senha/recupera.dart
+++ lib/View/Recuperar Login e Senha/recupera.dart
@@ -11,57 +11,84 @@ class RecuperaSenha extends StatefulWidget {
 class _RecuperaSenha extends State<RecuperaSenha> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFA901F7),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(top: 50),
-            height: 300,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(200),
-                  bottomRight: Radius.circular(200)),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 150,
-                  height: 150,
-                  child: Image.network(
-                      "https://i.giphy.com/media/XDdsJoGvQ8Z42M8s1h/giphy.webp"),
-                ),
-                Text(
-                  "Lovepeople",
-                  style: GoogleFonts.montserrat(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF3101B9)),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.center,
-              child: Image.asset(
-                'assets/image/beleza.png',
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFFA901F7),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 50),
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(200),
+                    bottomRight: Radius.circular(200)),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    child: Image.network(
+                        "https://i.giphy.com/media/XDdsJoGvQ8Z42M8s1h/giphy.webp"),
+                  ),
+                  Text(
+                    "Lovepeople",
+                    style: GoogleFonts.montserrat(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFF3101B9)),
+                  ),
+                  Container(
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacementNamed("login");
+                      },
+                      child: Text(
+                        "Voltar",
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Montserrat-SemiBold"),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Color(0xFF3101B9)),
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  side: BorderSide(
+                                      color: Colors.white, width: 2)))),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          Text(
-            'Boa sorte da Equipe-2 a voce',
-            style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontFamily: "Montserrat-SemiBold"),
-          ),
-        ],
+            Expanded(
+              child: Align(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/image/beleza.png',
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Vixi,esqueceu a senha!! , nem eu sei também kkk..Boa sorte!',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: "Montserrat-SemiBold"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
